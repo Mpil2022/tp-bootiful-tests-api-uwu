3955232b5a4ed50c42dfc1beb683c8889c815a24
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2022-01-08 11:30:30 +0100

    tests

--- src/test/java/uwu/adderapi/AdderApiApplicationTests.java
+++ src/test/java/uwu/adderapi/AdderApiApplicationTests.java
@@ -1,13 +1,32 @@
 package uwu.adderapi;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import uwu.adderapi.addition.AdderService;
+
+import static io.restassured.RestAssured.form;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 
 @SpringBootTest
 class AdderApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test() {
+        given()
+        .when()
+            .get("http://localhost:8080/adder/current")
+        .then()
+                .assertThat()
+                    .statusCode(HttpStatus.SC_OK)
+                    .contentType(ContentType.JSON);
     }
 
 }
