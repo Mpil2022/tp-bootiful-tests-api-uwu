da46de627b4cb4f73dbafbd4324b71e5ea0053ca
adeleb54 <adeleb54.2@gmail.com>
2022-01-08 09:31:37 +0100

    1st commit

---
+++ src/main/java/uwu/adderapi/AdderApiApplication.java
@@ -0,0 +1,13 @@
+package uwu.adderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uwu/adderapi/AdderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uwu.adderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
